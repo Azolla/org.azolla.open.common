20b208fd07cd1293a8ee742dadbb4d016f5013e2
ShaneKing <git@azolla.org>
2013-07-01 22:54:20 +0800

    add Encoding

---
+++ src/main/java/org/azolla/open/common/io/Encoding.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)Encoding.java		Created at 2013-7-1
+ * 
+ * Copyright (c) 2011-2013 azolla.org All rights reserved.
+ * Azolla PROPRIETARY/CONFIDENTIAL. Use is subject to license terms. 
+ */
+package org.azolla.open.common.io;
+
+/**
+ * The coder is very lazy, nothing to write for this Encoding class
+ *
+ * @author 	sk@azolla.org
+ * @since 	ADK1.0
+ */
+public class Encoding
+{
+	public static final String	UTF_8		= "UTF-8";
+
+	public static final String	GBK			= "GBK";
+
+	public static final String	ISO_8859_1	= "ISO-8859-1";
+}
